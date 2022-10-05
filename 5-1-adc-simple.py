e7c1d43b4cf45128adfd244680ffd451c607336a
SandraPatty <114083626+SandraPatty@users.noreply.github.com>
2022-10-05 16:54:35 +0300

    Add files via upload

--- 5-1-adc-simple.py
+++ 5-1-adc-simple.py
@@ -5,8 +5,8 @@ dac = [26, 19, 13, 6, 5, 11, 9, 10]
 comp = 4
 troyka = 17
 
-GPIO.setmode(GPIO.BCM)
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(dac, GPIO.OUT)
 GPIO.setup(troyka, GPIO.OUT, initial=1)
 GPIO.setup(comp, GPIO.IN)
@@ -15,18 +15,21 @@ def dec2bin(value):
     return [int(bit) for bit in bin(value)[2:].zfill(8)]
 
 def adc():
-    for _value in range(256):
-        GPIO.output(dac, dec2bin(_value))
+    for i in range(256):
+        GPIO.output(dac, dec2bin(i))
         sleep(0.0007)
-        _comp_value = GPIO.input(comp)
-        if _comp_value == 0:
-            return _value
+        c = GPIO.input(comp)
+        if c == 0:
+            return i
+
 
 try:
     while True:
         value = adc()
-        voltage = 3.3 / 256 * value
-        print('{:.2f}'.format(voltage), 'V', value)
+        volt = 3.3 / 256 * value
+        print('{:.2f}'.format(volt), 'V', value)
+
+    
 finally:
     GPIO.output(dac, 0)
     GPIO.cleanup()
